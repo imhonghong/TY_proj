2d0c7a4768ff78acfb7a9a4b4c72c27108f02e27
imhonghong <131515657+imhonghong@users.noreply.github.com>
2024-01-22 19:38:20 +0800

    Update IDC.py

--- py/IDC.py
+++ py/IDC.py
@@ -2,12 +2,24 @@
 
 def read_img(file_path):
 	img_in=[]
-	f=open.(file_path,'r')
+	f=open(file_path,'r')
 	img_in=f.readlines()
 	f.close
 	return img_in
 	#input : str
 	#output: 1D list
 
-img_in=read_img(r'py/img1.txt')
-print(img_in)
+def print_img(img_in):
+	for i in range(0,64):
+		if(img_in[i]>99):
+			print(img_in[i])
+		elif((img_in[i]<99)&&(img_in[i]>9)):
+			print(f" {img_in[i]}")
+		else:
+			print(f"  {img_in[i]}")
+		if( i%8==0 && i!=0 ):
+			print(\n)
+
+img_in_path=r'py/img1.txt'
+img_in=read_img(img_in_path)
+print_img(img_in)
