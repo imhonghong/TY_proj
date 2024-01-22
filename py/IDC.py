9d9ed000fd2a88343d03110824a2ff12d80346d2
imhonghong <131515657+imhonghong@users.noreply.github.com>
2024-01-22 19:25:21 +0800

    Update IDC.py

--- py/IDC.py
+++ py/IDC.py
@@ -1,12 +1,14 @@
 #using python to solve first??
 
-img_in=[]
-
-path_in=r'py/img1.txt'
-f=open.(path_in,'r')
-img_in=f.readlines()
-f.close
-
-print(img_in)
-
 def read_img(file_path):
+	img_in=[]
+	f=open.(file_path,'r')
+	img_in=f.readlines()
+	f.close
+	return img_in
+	#input : str
+	#output: 1D list
+
+
+img_in=read_img(r'py/img1.txt')
+print(img_in)
